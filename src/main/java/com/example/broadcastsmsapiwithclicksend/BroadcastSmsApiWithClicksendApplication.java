3e35fd0d781a4fc43b47d0d9c51b68f34b3ab8f2
kikiodazie <dodazie@gmail.com>
2022-06-26 22:32:32 +0100

    Initial Commit

---
+++ src/main/java/com/example/broadcastsmsapiwithclicksend/BroadcastSmsApiWithClicksendApplication.java
@@ -0,0 +1,13 @@
+package com.example.broadcastsmsapiwithclicksend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastSmsApiWithClicksendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroadcastSmsApiWithClicksendApplication.class, args);
+    }
+
+}
